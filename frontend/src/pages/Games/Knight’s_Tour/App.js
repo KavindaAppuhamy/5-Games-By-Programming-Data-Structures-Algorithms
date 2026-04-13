970474573ea45e45c296480d81ddcdb4422d2358
Iffathnazeer <iffathnazeer2002@gmail.com>
2026-04-13 17:00:46 +0530

    app js

---
+++ frontend/src/pages/Games/Knight’s_Tour/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import KnightsTourGame from './components/KnightsTourGame';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <KnightsTourGame />
+    </div>
+  );
+}
+
+export default App;
