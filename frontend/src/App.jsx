737136e883223e129e90755121d4f3e7795b75b6
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2026-04-13 14:28:07 +0530

    update app layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,55 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import GameMenu from './pages/Menu/gameMenu'
-import MinCost from './pages/Games/Minimum_Cost_Assignment/MinCost'
-import SnakeLadder from './pages/Games/Snake_&_Ladder/SnakeLadder'
-import Traffic from './pages/Games/Traffic_Simulation/Traffic'
-import KnightTour from './pages/Games/Knight’s_Tour/KnightTour'
-import SixteenQueens from './pages/Games/Sixteen_Queens/SixteenQueens'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
+import './App.css';
+
+import GameMenu from './pages/Menu/gameMenu';
+import MinCost from './pages/Games/Minimum_Cost_Assignment/MinCost';
+import SnakeLadder from './pages/Games/Snake_&_Ladder/SnakeLadder';
+import Traffic from './pages/Games/Traffic_Simulation/Traffic';
+import KnightTour from './pages/Games/Knight’s_Tour/KnightTour';
+import SixteenQueens from './pages/Games/Sixteen_Queens/SixteenQueens';
 
 function App() {
- 
-
   return (
-    <><BrowserRouter>
-       <Routes>
-        <Route path="/" element={<GameMenu />} />
-        <Route path="/min-cost" element={<MinCost />} />
-        <Route path="/snake-ladder" element={<SnakeLadder />} />
-        <Route path="/traffic" element={<Traffic />} />
-        <Route path="/knight-tour" element={<KnightTour />} />
-        <Route path="/sixteen-queens" element={<SixteenQueens />} />
-      </Routes>
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: '#12121f',
+            color: '#e8e8f0',
+            border: '1px solid #1e1e35',
+            fontFamily: 'JetBrains Mono, monospace',
+            fontSize: '13px',
+          },
+          success: {
+            iconTheme: {
+              primary: '#00ff88',
+              secondary: '#0a0a12',
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: '#ff4466',
+              secondary: '#0a0a12',
+            },
+          },
+        }}
+      />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GameMenu />} />
+          <Route path="/min-cost" element={<MinCost />} />
+          <Route path="/snake-ladder" element={<SnakeLadder />} />
+          <Route path="/traffic" element={<Traffic />} />
+          <Route path="/knight-tour" element={<KnightTour />} />
+          <Route path="/sixteen-queens" element={<SixteenQueens />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
