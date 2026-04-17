e6cc6ef6a4c9dd18e7f924fa6fe394b21082a08a
Iffathnazeer <iffathnazeer2002@gmail.com>
2026-04-17 23:07:34 +0530

    name edited game repo

--- backend/src/main/java/com/pdsagame/backend/KnightsTour/repositories/GameRepository.java
+++ backend/src/main/java/com/pdsagame/backend/KnightsTour/repositories/GameRepository.java
@@ -1,6 +1,6 @@
 package com.pdsagame.backend.KnightsTour.repositories;
 
-import com.pdsagame.backend.knightstour.entity.GameSession;
+import com.pdsagame.backend.KnightsTour.entity.GameSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
