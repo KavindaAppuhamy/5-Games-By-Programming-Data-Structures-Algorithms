15302430f1540da1b37c5c200f8c0dd648dd25a0
Iffathnazeer <iffathnazeer2002@gmail.com>
2026-04-17 23:12:30 +0530

    name

--- backend/src/main/java/com/pdsagame/backend/KnightsTour/controller/KnightsTourController.java
+++ backend/src/main/java/com/pdsagame/backend/KnightsTour/controller/KnightsTourController.java
@@ -1,6 +1,6 @@
 package com.pdsagame.backend.KnightsTour.controller;
 
-import com.knightstour.service.KnightsTourService;
+import com.pdsagame.backend.KnightsTour.service.KnightsTourService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
