9dbbc61187300e4c375b7c871ee8987319478c74
Kavinda <kavinda.appuhamy@gmail.com>
2026-04-17 23:48:26 +0530

    Revert "Merge branch 'Dev' into Traffic-Simulation-Game"
    This reverts commit 30c031a36fea5ca03546195ead4e067a9477f968, reversing
    changes made to 9ef8dbf13ae917e04f133cff729006a28ea14c9a.

--- backend/src/main/java/com/pdsagame/backend/queens/exception/SolverException.java
+++
@@ -1,5 +0,0 @@
-package com.pdsagame.backend.queens.exception;
-
-public class SolverException extends RuntimeException {
-    public SolverException(String message, Throwable cause) { super(message, cause); }
-}
