24ad99ceaa3266a154a49744b584dd1cf5fc8963
Kavinda <kavinda.appuhamy@gmail.com>
2026-04-20 12:36:56 +0530

    Reapply "Merge branch 'Traffic-Simulation-Game' into Dev"
    This reverts commit b6b5c9cb8518e1f28a88ae5eb442a1fb889157ea.

--- backend/src/main/java/com/pdsagame/backend/SnakeLadderGame/exception/InvalidBoardConfigException.java
+++
@@ -1,7 +0,0 @@
-package com.pdsagame.backend.SnakeLadderGame.exception;
-
-public class InvalidBoardConfigException extends RuntimeException {
-    public InvalidBoardConfigException(String message) {
-        super(message);
-    }
-}
